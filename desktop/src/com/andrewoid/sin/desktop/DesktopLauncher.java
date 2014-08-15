35c7586d2c31e9109bdab2723a4fc6db96e435c7
Andrew Nash <andrew@andrewoid.co.uk>
2014-08-15 17:46:43 +0100

    Initial project

---
+++ desktop/src/com/andrewoid/sin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.andrewoid.sin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.andrewoid.sin.SinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SinGame(), config);
+	}
+}
---
+++ ios/src/com/andrewoid/sin/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.andrewoid.sin;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.andrewoid.sin.SinGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SinGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
